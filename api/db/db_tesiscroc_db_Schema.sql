1985d4c37ea9a1959e61a25eef1a4a9ad1c5d281
gianmarcoprando@gmail.com <gianmarcoprando@gmail.com>
2020-03-16 20:11:08 +0000

    File generated by Skaffolder - user: gianmarcoprando@gmail.com

---
+++ api/db/db_tesiscroc_db_Schema.sql
@@ -0,0 +1,65 @@
+--
+-- Database: `tesiscroc_db`
+--
+
+CREATE DATABASE IF NOT EXISTS `tesiscroc_db`;
+USE `tesiscroc_db`;
+
+
+-- ENTITIES
+
+--
+-- Struttura della tabella `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+	`cognome` varchar(130)  NOT NULL,
+	`mail` varchar(130)  NOT NULL,
+	`nome` varchar(130)  NOT NULL,
+	`password` varchar(130)  NOT NULL,
+	`username` varchar(130)  NOT NULL,
+	
+	`_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT 
+
+);
+
+
+-- Security
+
+ALTER TABLE `user` MODIFY COLUMN `password` varchar(128)  NOT NULL;
+
+INSERT INTO `tesiscroc_db`.`user` (`username`, `password`, `_id`) VALUES ('admin', '62f264d7ad826f02a8af714c0a54b197935b717656b80461686d450f7b3abde4c553541515de2052b9af70f710f0cd8a1a2d3f4d60aa72608d71a63a9a93c0f5', 1);
+
+CREATE TABLE IF NOT EXISTS `roles` (
+	`role` varchar(30) ,
+	
+	-- RELAZIONI
+
+	`_user` int(11)  NOT NULL REFERENCES user(_id),
+	`_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT 
+
+);
+INSERT INTO `tesiscroc_db`.`roles` (`role`, `_user`, `_id`) VALUES ('ADMIN', '1', 1);
+
+
+--
+-- Struttura della tabella `coppia`
+--
+
+CREATE TABLE IF NOT EXISTS `coppia` (
+	
+	`_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT 
+
+);
+
+
+
+
+
+-- relation 1:m gioca coppia - User
+ALTER TABLE `coppia` ADD COLUMN `gioca` int(11)  REFERENCES user(_id);
+
+-- relation 1:m giocano coppia - User
+ALTER TABLE `coppia` ADD COLUMN `giocano` int(11)  REFERENCES user(_id);
+
+
